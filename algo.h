848eeb13928b343c13d079cb04706fcb146890de
dyxg <45059592+dyxg@users.noreply.github.com>
2022-09-28 15:39:11 +0800

    Add files via upload
    添加了代码注释，删除了debug代码，更换了量化表

--- algo.h
+++ algo.h
@@ -6,7 +6,7 @@
 #include<cstdio>
 #include<vector>
 #include<cstring>
-
+#include<queue>
 #include "IO.h"
 
 
@@ -47,7 +47,7 @@ void DCT(double matrix[block][block]) {
     }
 }
 
-
+/*
 const int Qy[block][block]={
     {16,11,10,16,24,40,51,61},
     {12,12,14,19,26,58,60,55},
@@ -58,9 +58,20 @@ const int Qy[block][block]={
     {49,64,78,87,103,121,120,101},
     {72,92,95,98,112,100,103,99}
 };
+*/
+const int Qy[block][block] = {
+    {0x08,0x06,0x06,0x07,0x06,0x05,0x08,0x07},
+    {0x07,0x07,0x09,0x09,0x08,0x0A,0x0C,0x14},
+    {0x0D,0x0C,0x0B,0x0B,0x0C,0x19,0x12,0x13},
+    {0x0F,0x14,0x1D,0x1A,0x1F,0x1E,0x1D,0x1A},
+    {0x1C,0x1C,0x20,0x24,0x2E,0x27,0x20,0x22},
+    {0x2C,0x23,0x1C,0x1C,0x28,0x37,0x29,0x2C},
+    {0x30,0x31,0x34,0x34,0x34,0x1F,0x27,0x39},
+    {0x3D,0x38,0x32,0x3C,0x3E,0x33,0x34,0x32}
+};
 
 //   Y通道量化矩阵
-
+/*
 const int Qc[block][block] {
     {17,18,24,47,99,99,99,99},
     {18,21,26,66,99,99,99,99},
@@ -70,6 +81,17 @@ const int Qc[block][block] {
     {99,99,99,99,99,99,99,99},
     {99,99,99,99,99,99,99,99},
     {99,99,99,99,99,99,99,99}
+};
+*/
+const int Qc[block][block] {
+    {0x09,0x09,0x09,0x0C,0x0B,0x18,0x0D,0x0D},
+    {0x18,0x32,0x21,0x1C,0x21,0x32,0x32,0x32},
+    {0x32,0x32,0x32,0x32,0x32,0x32,0x32,0x32},
+    {0x32,0x32,0x32,0x32,0x32,0x32,0x32,0x32},
+    {0x32,0x32,0x32,0x32,0x32,0x32,0x32,0x32},
+    {0x32,0x32,0x32,0x32,0x32,0x32,0x32,0x32},
+    {0x32,0x32,0x32,0x32,0x32,0x32,0x32,0x32},
+    {0x32,0x32,0x32,0x32,0x32,0x32,0x32,0x32}
 };
 
 
@@ -130,6 +152,9 @@ void ZigZag(int line[block*block],int source[block][block]) {
     }
 }
 
+
+// 编码计算
+
 inline unsigned int encodelength(int val) {
     unsigned int res=0;
     for(val=abs(val);val;val>>=1) res++;
@@ -144,13 +169,19 @@ inline int encodeval(int val) {
 
 int count[256];
 std::pair< std::vector<int> ,std::vector<int> > __huffman() {
+
+    // 哈夫曼表生成
+    // 每次取出两个出现次数最小数A,B的合并为一个C，AB的码长为C的码长+1
+    
+
     std::priority_queue < std::pair<int,int>, std::vector< std::pair<int,int> > ,std::greater <std::pair<int,int> > > que;
+    // 构造优先队列
     int Fa[256*2],Len[256*2],cnt=256;
     memset(Fa,-1,sizeof Fa);
     for(int i=0;i<256;i++) if (count[i]) {
         que.push(std::make_pair(count[i],i));
     }
-    
+
     while(que.size()>1) {
         std::pair<int,int> A=que.top();
         que.pop();
@@ -161,21 +192,40 @@ std::pair< std::vector<int> ,std::vector<int> > __huffman() {
         que.push(std::make_pair(A.first+B.first,cnt));
         cnt++;
     }
-    // exit(0);
+    // 构造哈夫曼树
+
     std::vector<int> A,B;
     std::vector<int> C[17];
     A.resize(17);
-
+    std::vector<int> Rb;
     for(int i=0;i<256;i++) if (count[i]) {
         int l=0;
         for(int j=i;j!=-1;j=Fa[j]) {
             l++;
         }
-        // l--;
-        assert(l<=16);
-        if (l==1) l++;
-        A[l]++;C[l].push_back(i);
+        // 计算码长
+        if (l>16) {
+            Rb.push_back(i);
+            continue;
+        }     
+        /* 特别说明：
+            范式哈夫曼表不能出现码长>16的情况
+            但是在哈夫曼表构造直接中可能会出现
+            这里将>16的编码回收
+        */
+        if (l==1) l++;   // 哈夫曼表避免码长为1的情况（我也不知道为什么这样会挂）
+        A[l]++;C[l].push_back(i);    // 计算每个码长对应的编码数
     }
+    for(int now=0;!Rb.empty();now--,A[16]++,C[16].push_back(Rb.back()),Rb.pop_back()) {
+        for(int j=15;j>=1;j--) if (A[j]) {
+            A[j]--;A[16]++;
+            now+=1<<(16-j);
+            now--;
+        }
+    }
+    /*
+        去掉一个较短的码然后可以放入一个被回收的元素
+    */
     for(int i=0;i<=16;i++) {
         for(auto j:C[i]) {
             B.push_back(j);
@@ -187,9 +237,9 @@ std::pair< std::vector<int> ,std::vector<int> > __huffman() {
 std::pair< std::vector<int> ,std::vector<int> > Huffman(std::vector< encode > source) {
     memset(count,0,256*4);
     for(auto i:source) {
-        // assert(i.length<256);
         count[i.length]++;
     }
+    // 统计出现次数
     return __huffman();
 }
 
@@ -201,20 +251,16 @@ std::pair< std::vector<int> ,std::vector<int> > Huffman(std::vector< encode > so
 }
 
 void printmap(std::pair< std::vector<int> ,std::vector<int> > source) {
-    bool tag=0;
-    // int ccc=0;
     for(int i=1;i<=16;i++) {
         Putchar(source.first[i]);   // 各长度数量
-        // ccc+=source.first[i];
     }
-    // int ddd=0;
     for(auto i:source.second) Putchar(i);    //  范式哈夫曼编码与原值对应关系
-    // assert(ccc==ddd);
 }
 struct huffmanmap {
     int iter=0,cnt=0;
     int c[256],Len[256],huffmanval[256];
     huffmanmap(std::pair< std::vector<int> ,std::vector<int> > table) {
+        // 构造范式哈夫曼表
         c[0]=0;
         for(auto v:table.first) {
             for(int j=1;j<=v;j++) {
@@ -223,7 +269,6 @@ struct huffmanmap {
                     else c[iter]=((c[iter-1]+1)<<(cnt-Len[iter-1]));
                 }
                 Len[iter]=cnt;
-                // assert(Len[iter]>=encodelength(c[iter]));
                 iter++;
             }
             cnt++;
@@ -231,20 +276,11 @@ struct huffmanmap {
         iter=0;
         memset(huffmanval,-1,256*4);
         for(auto it:table.second) {
-            #ifdef DEBUG
-            std::cerr << Len[iter] <<" " << c[iter] << " " << it << std::endl;
-            #endif
             huffmanval[it]=iter++;
         }
-        #ifdef DEBUG
-        std::cerr << std::endl;
-        #endif
     }
     std::pair<int,int> query(int v) {
-        if (huffmanval[v]==-1) {
-            std::cerr << "error " << v << std::endl;
-            exit(0);
-        }
+        // 询问该值的对应长度与编码
         return std::make_pair(Len[huffmanval[v]],c[huffmanval[v]]);
     }
 };
@@ -252,34 +288,23 @@ struct huffmanmap {
 int Cnt=0;
 
 void outputblock(huffmanmap &mapDC,huffmanmap &mapAC,std::vector<encode>::iterator &DC,std::vector<encode>::iterator &AC) {
-    
+    // 单个块输出
     push(mapDC.query(DC->length).first,mapDC.query(DC->length).second);
-    // std::cerr << mapDC.query(itdc->length).first << std::endl;
-    #ifdef DEBUG
-    // std::cerr << mapDC.query(DC->length).first << " " << mapDC.query(DC->length).second << " " << DC->length << " " << DC->v << std::endl;
-    #endif
     push(DC->length,DC->v);
     int total=0;
     for(;;++AC) {
-        // Cnt++;
-        // std::cerr << Cnt << std::endl;
-        #ifndef DEBUG
         push(mapAC.query(AC->length).first,mapAC.query(AC->length).second);
-        #else
-        Putchar(AC->length);
-        #endif
-        // std::cerr << mapAC.query(AC->length).first << " " << mapAC.query(AC->length).second << " " << AC->length << " " << AC->v << std::endl;
-        // std::cerr << itac -> length << std::endl;
         push(AC->length&15,AC->v);
         if (AC->length==0x00) break;
         total+=AC->length>>4;
         total++;
+        // 把整个AC部分输出
     }
-    assert(total<=63);
+    // assert(total<=63);
     ++DC;++AC;
 }
 
-void printhuffman(std::pair< std::vector<int> ,std::vector<int> > table1DC,
+void printresult(std::pair< std::vector<int> ,std::vector<int> > table1DC,
                     std::pair< std::vector<int> ,std::vector<int> > table1AC,
                     std::pair< std::vector<int> ,std::vector<int> > table2DC,
                     std::pair< std::vector<int> ,std::vector<int> > table2AC,
@@ -287,7 +312,9 @@ void printhuffman(std::pair< std::vector<int> ,std::vector<int> > table1DC,
                     std::pair< std::vector<encode> ,std::vector< encode > >  dataCb,
                     std::pair< std::vector<encode> ,std::vector< encode > >  dataCr
                     ) {
+        // 输出最终数据
         huffmanmap mapYDC(table1DC),mapYAC(table1AC),mapCDC(table2DC),mapCAC(table2AC);
+
         std::vector<encode>::iterator YDC=dataY.first.begin();
         std::vector<encode>::iterator CbDC=dataCb.first.begin();
         std::vector<encode>::iterator CrDC=dataCr.first.begin();
@@ -300,6 +327,7 @@ void printhuffman(std::pair< std::vector<int> ,std::vector<int> > table1DC,
             outputblock(mapYDC,mapYAC,YDC,YAC);
             outputblock(mapCDC,mapCAC,CrDC,CrAC);
             outputblock(mapCDC,mapCAC,CbDC,CbAC);
+            
         }
     }
 }
--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
-#include<bits/stdc++.h>
+#include<cstdio>
+#include<vector>
+#include<algorithm>
+#include<cstring>
 
 #include "algo.h"
 #include "IO.h"
@@ -19,31 +22,30 @@ const unsigned int Maxlength = 15;
 #define YG 0.587
 #define YB 0.114
 
-#define CbR 0.4998
-#define CbG -0.4185
+#define CbR 0.5
+#define CbG -0.4187
 #define CbB -0.0813
 
 #define CrR -0.1687
 #define CrG -0.3313
-#define CrB 0.4997
+#define CrB 0.5
 
 struct picture {
     
-    std::string name;
 
 
     unsigned char R[SIZE][SIZE],G[SIZE][SIZE],B[SIZE][SIZE];
 
     unsigned int Xpixel = SIZE, Ypixel=SIZE,Xdensity=64,Ydensity=64;
 
-    std::pair<std::vector < encode > ,std::vector< encode > > Y;
-    std::pair<std::vector < encode > ,std::vector< encode > > Cr;
-    std::pair<std::vector < encode > ,std::vector< encode > > Cb;
+    std::pair<std::vector < encode > ,std::vector< encode > > Y;          // Y图层游程编码
+    std::pair<std::vector < encode > ,std::vector< encode > > Cr;         // Cr图层游程编码
+    std::pair<std::vector < encode > ,std::vector< encode > > Cb;         // Cb图层游程编码
 
-    std::pair< std::vector<int> ,std::vector<int> > YDC;
-    std::pair< std::vector<int> ,std::vector<int> > YAC;
-    std::pair< std::vector<int> ,std::vector<int> > CDC;
-    std::pair< std::vector<int> ,std::vector<int> > CAC;
+    std::pair< std::vector<int> ,std::vector<int> > YDC;                   // Y图层DC哈夫曼表
+    std::pair< std::vector<int> ,std::vector<int> > YAC;                   // Y图层AC哈夫曼表
+    std::pair< std::vector<int> ,std::vector<int> > CDC;                   // C图层DC哈夫曼表
+    std::pair< std::vector<int> ,std::vector<int> > CAC;                   // C图层AC哈夫曼表
 
     std::pair<std::vector < encode > ,std::vector< encode > >
                 TransToVector(double ValR,double ValG,double ValB,double ValC,int option) {   
@@ -62,32 +64,10 @@ struct picture {
                     for(int j1=j;j1<j+block;j1++) {
                         tmp[i1-i][j1-j]=(ValR*R[i1][j1]+ValG*G[i1][j1]+ValB*B[i1][j1]+ValC-128);
                         
-                        // if (!(tmp[i1-i][j1-j]>=-128&&tmp[i1-i][j1-j]<=127)) {
-                            
-                        //     exit(0);
-                        // }
                     }
                 }
                 algorithm::DCT(tmp);
-                #ifdef DEBUG
-                // for(int u=0;u<block;u++) {
-                //     for(int v=0;v<block;v++) {
-                //         printf("%.2lf ",tmp[u][v]);
-                //     }
-                //     putchar('\n');
-                // }
-                // putchar('\n');
-                #endif
                 algorithm::quantify(result,tmp,option);
-                #ifdef DEBUG
-                for(int u=0;u<block;u++) {
-                    for(int v=0;v<block;v++) {
-                        printf("%d ",result[u][v]);
-                    }
-                    putchar('\n');
-                }
-                putchar('\n');
-                #endif
                 algorithm::ZigZag(line,result);
                 // 对单位8*8矩阵进行处理，将该8*8的小矩形量化
                 {
@@ -97,7 +77,7 @@ struct picture {
                 }
                 // 差分
                 DC.push_back((encode){(algorithm::encodelength(line[0])),algorithm::encodeval(line[0])});
-                // printf("%d %d\n",algorithm::encodelength(line[0]),algorithm::encodeval(line[0]));
+
                 for(unsigned int iterator=1,nowzero=0;iterator<block*block;iterator++) {
                     if (line[iterator]==0&&nowzero<Maxlength) nowzero++;
                     else {
@@ -110,29 +90,21 @@ struct picture {
                 AC.push_back(encode{0,0});
             }
         }
-        #ifdef DEBUG
-        // for(auto i:AC) {
-        //     printf("(%d,%d),%d\n",i.length>>4,i.length&15,i.v);
-        // }
-        #endif
-        // printf("%d %d\n",AC.size(),Cnt);
+
         return std::make_pair(DC,AC);
     }
     void Trans() {
         Y=TransToVector(YR,YG,YB,0,0);
         Cb=TransToVector(CbR,CbG,CbB,128,1);
         Cr=TransToVector(CrR,CrG,CrB,128,1);
-        // for(auto i:Y.second) {
-        //     Putchar(i.length),Putchar(i.v);
-        // }
+        // 完成编码
         YDC=algorithm::Huffman(Y.first);
         YAC=algorithm::Huffman(Y.second);
-        // exit(0);
         CDC=algorithm::Huffman(Cr.first,Cb.first);
         CAC=algorithm::Huffman(Cr.second,Cb.second);
-        // exit(0);
+        // 哈夫曼表构造
     }
-    void OutputMap(int HT,std::pair< std::vector<int> ,std::vector<int> > &Map) {
+    void OutputMap(int HT,std::pair< std::vector<int> ,std::vector<int> > &Map) {    // 输出哈夫曼表
         Putchar(0xFF),Putchar(0xC4);
         unsigned int Len=Map.second.size()+19;
         Putchar(Len>>8),Putchar(Len&255);//段长
@@ -140,9 +112,7 @@ struct picture {
         algorithm::printmap(Map);
     }
     void Output(char *file) {
-        #ifndef DEBUG
         freopen(file,"w",stdout);
-        #endif
         Putchar(0xFF),Putchar(0xD8);
         // 文件头
 
@@ -155,11 +125,7 @@ struct picture {
         Putchar(Ydensity>>8),Putchar(Ydensity&255);//像素密度
         Putchar(0),Putchar(0);
         // APP0图像识别信息
-        #ifdef DEBUG
-        CNT=0;
-        putchar('\n');
-        printf("APP0\n");
-        #endif
+
         Putchar(0xFF),Putchar(0xDB);
         Putchar(0x00),Putchar(0x43);
         Putchar(0x00);
@@ -169,11 +135,6 @@ struct picture {
         Putchar(0x01);
         algorithm::Qout(1);
         // DQT定义量化表
-        #ifdef DEBUG
-        CNT=0;
-        putchar('\n');
-        printf("DQT\n");
-        #endif
 
 
         Putchar(0xFF),Putchar(0xC0);
@@ -187,34 +148,11 @@ struct picture {
         Putchar(0x03),Putchar(0x11),Putchar(0x01);//Cr
         //SOF0图像基本信息
 
-        #ifdef DEBUG
-        CNT=0;
-        putchar('\n');
-        printf("SOF0\n");
-        #endif
         OutputMap(0x00,YDC);
-        #ifdef DEBUG
-        CNT=0;
-        putchar('\n');
-        #endif
         OutputMap(0x10,YAC);
-        #ifdef DEBUG
-        CNT=0;
-        putchar('\n');
-        #endif
         OutputMap(0x01,CDC);
-        #ifdef DEBUG
-        CNT=0;
-        putchar('\n');
-        #endif
         OutputMap(0x11,CAC);
         //哈夫曼编码表
-        
-        #ifdef DEBUG
-        CNT=0;
-        putchar('\n');
-        printf("Huffman\n");
-        #endif
         
         Putchar(0xFF),Putchar(0xDA);
         Putchar(0x00),Putchar(0x0C);
@@ -225,8 +163,8 @@ struct picture {
         Putchar(0x00);Putchar(0x3F);Putchar(0x00);
         
         // SOS扫描行
-        algorithm::printhuffman(YDC,YAC,CDC,CAC,Y,Cb,Cr);
-        // std::cerr << algorithm :: Cnt << std::endl;
+        algorithm::printresult(YDC,YAC,CDC,CAC,Y,Cb,Cr);    // 输出数据
+
         push(255,1);
 
         Putchar(0xFF),Putchar(0xD9);
@@ -261,10 +199,5 @@ void Transform(char *file_in,char *file_out) {
 }
 
 int main(int argc,char *argv[]) {
-    srand(time(0));
-    #ifdef DEBUG
-    freopen("error.out","w",stderr);
-    freopen("test.out","w",stdout);
-    #endif
     Transform("demo.bin","demo.jpeg");
 }
